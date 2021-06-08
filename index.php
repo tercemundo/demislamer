aa6a436744e93ab43ce2d47cd93f1687958f6d1a
mguazzardo <mguazzardo76@gmail.com>
2021-06-07 20:34:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
- $mysqli = new mysqli( mariadb , 'root', 'master', 'sampledb');
- // If an error occurs when establishing the connection
- if ($mysqli->connect_errno) {
- // We can show a message:
- echo "An error occurred when trying to establish a
-connection to the database: ";
- // And this is the detail of the error message.
- echo "Error " . "#".$mysqli->connect_errno . "\n";
- exit;
- }
- else {
- // Query to retrieve Orders table data
- $select_query = "SELECT id, name, supplier, category, price,
-instock FROM products";
- $result = $mysqli->query($select_query);
- while ($obj = mysqli_fetch_object($result)) {// call to
- echo "ID: ".$obj->id.", ".
- "Name: ".$obj->name.", ".
- "Supplier: ".$obj->supplier.", ".
- "Category: ".$obj->category.", ".
- "Price: ".$obj->price.", ".
- "In Stock: ".$obj->instock."<br>";
- }
- }
+phpinfo();
 ?>
  
 
